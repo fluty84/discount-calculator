2082556c01490a9dbf63d6e71ba5dc358b668d32
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2021-12-23 13:55:46 +0100

    first commit

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Calculadora suma y resta porcentajes
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/fluty84-the-bashful/pen/poWoxBq](https://codepen.io/fluty84-the-bashful/pen/poWoxBq).
+
+
---
+++ script.js
@@ -0,0 +1,47 @@
+function calculaPorciento(valor,porcentaje){
+  return valor*(porcentaje*0.01)
+  
+}
+
+//console.log(calculaPorciento(100,8))
+
+
+function sumaPorciento(valor, porcentaje){
+  return valor + calculaPorciento(valor, porcentaje)
+}
+
+//console.log(sumaPorciento(100,8))
+
+function restaPorciento(valor, porcentaje){
+  return valor - calculaPorciento(valor, porcentaje)
+}
+
+function porcent(valor, porcentaje, operador){
+  switch(operador){
+    case "+":
+      alert (sumaPorciento(valor, porcentaje))
+      break;
+    case "-": 
+      return restaPorciento(valor, porcentaje)
+  }
+}
+
+document.querySelector("button").onclick = ejecutar;
+
+function ejecutar(){
+  let initialElement = document.querySelector('#initial').value;
+  let initial = parseFloat(initialElement)
+  console.log(initial)
+  
+  let percentElement = document.querySelector('#percent').value;
+  let percent = parseFloat(percentElement)
+  console.log(percent)
+  
+  let operator = document.querySelector("select option").value;
+  console.log(operator)
+  
+  porcent(initial, percent, operator)
+ 
+}
+
+//console.log(porcent(100,8,"+"))
